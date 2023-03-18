1d6b68a347b1856f3ec40856343ae88c02c96b23
dev-parthive <dev-parthive>
2023-03-18 23:19:05 +0600

    signup with email done

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,11 +5,13 @@ import Dashboard from "../pages/Dashboard/Dashboard";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
+import ErrorPage from "../Shared/ErrorPage/ErrorPage";
 
 const routes = createBrowserRouter([
     {
         path: '/', 
         element: <Main></Main>, 
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/', 
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,16 +1,40 @@
 import React, { createContext, useState } from 'react';
-import {getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile} from 'firebase/auth'
 import app from '../Firebase/Firebase.config';
 export const AuthContext = createContext()
 const auth = getAuth(app)
 const AuthProvider = ({children}) => {
-    const [user, setUser] = useState({
-      email: "parthive"
-    })
+  // state for sotre user 
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    
+
+
+  //1.create user 
+const createUser = (email, password)=>{
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+// 2.update name 
+const  updateUser = (name, photo)=>{
+  return updateProfile(auth.currentUser , {
+    displayName: name,
+    photoURL: photo
+  })
+}
+// 3. Email verify 
+const verifyEmail = ()=>{
+  return sendEmailVerification(auth.currentUser)
+}
+
+
+    
     
     const authInfo = {
         user, 
         setUser, 
+        createUser, 
+        updateUser, 
+        verifyEmail
     }
     return (
       <AuthContext.Provider value={authInfo}>
